2945df7a63aaf638c159974b602ef1a0e4b6dfcf
Lab. Desenvolvimento <Lab. Desenvolvimento@172.31.0.202>
2017-03-02 20:08:06 -0300

    versao 1.0 - versao inicial

---
+++ teste/src/opet/alomundo.java
@@ -0,0 +1,13 @@
+package opet;
+
+public class alomundo
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+           System.out.print("alomundo");
+
+    }
+
+}
